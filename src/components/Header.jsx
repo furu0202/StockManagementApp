62a517f06e13d3c34299998b021af6b1244c3216
furu0202 <furuka.0202@gmail.com>
2023-02-24 00:12:17 +0900

    コンポーネント作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* ========= 戻るエリア ======== */}
+      <div></div>
+
+      {/* ======== タイトル画面エリア ======== */}
+      <div></div>
+
+      {/* ======== 検索画面エリア ======== */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
