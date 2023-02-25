a1b55ef2647d67530cb5f8429f34fa5241610640
furu0202 <furuka.0202@gmail.com>
2023-02-26 02:21:45 +0900

    インプットタグの導入

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,19 @@ import 'src/styles/header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className='siteHeader'>
       {/* ========= 戻るエリア ======== */}
-      <div></div>
+      <div className={'goBackButton'}>TOP画面へ戻る</div>
 
       {/* ======== タイトル画面エリア ======== */}
-      <div className={'test'}>商品管理画面</div>
+      <div className={'title'}>商品管理画面</div>
 
       {/* ======== 検索画面エリア ======== */}
-      <div></div>
+      <input
+        type='search'
+        className='text'
+        placeholder='商品名を入力してください。'
+      />
     </div>
   );
 };
