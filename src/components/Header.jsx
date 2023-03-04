c1f295de2faa8766d197a19bdcc70a7082151e88
furu0202 <furuka.0202@gmail.com>
2023-03-05 02:54:39 +0900

    className 記述方法を統一

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,18 @@ import 'src/styles/header.css';
 import { GrSearch } from 'react-icons/gr';
 const Header = () => {
   return (
-    <div className='siteHeader'>
+    <div className={'siteHeader'}>
       {/* ========= 戻るエリア ======== */}
       <div className={'goBackButton'}>TOP画面へ戻る</div>
       {/* ======== タイトル画面エリア ======== */}
       <div className={'titleArea'}>商品管理画面</div>
       {/* ======== 検索画面エリア ======== */}
       <div className={'searchArea'}>
-        <GrSearch className='GrSearch' />
+        <GrSearch className={'GrSearch'} />
         <input
           type='search'
-          className='text'
-          placeholder='商品名を入力してください。'
+          className={'text'}
+          placeholder={'商品名を入力してください。'}
         />
       </div>
     </div>
