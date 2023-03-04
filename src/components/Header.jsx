2fa0b502670dc34d9d86420bbad6f983641397cf
furu0202 <furuka.0202@gmail.com>
2023-03-04 17:33:25 +0900

    各エリアの要素名を設定

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,14 @@ const Header = () => {
     <div className='siteHeader'>
       {/* ========= 戻るエリア ======== */}
       <div className={'goBackButton'}>TOP画面へ戻る</div>
-
+      <p>
+        <img src='img/left-icon.png' alt=''></img>
+      </p>
       {/* ======== タイトル画面エリア ======== */}
-      <div className={'title'}>商品管理画面</div>
+      <div className={'titleArea'}>商品管理画面</div>
 
       {/* ======== 検索画面エリア ======== */}
+      <div className={'searchArea'}>商品検索画面</div>
       <input
         type='search'
         className='text'
