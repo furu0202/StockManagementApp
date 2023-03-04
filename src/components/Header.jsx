bd25fb655f5573a4e844b9c60eec7a0cd902651a
furu0202 <furuka.0202@gmail.com>
2023-03-05 02:30:42 +0900

    検索アイコン実装

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import 'src/styles/header.css';
-
+import { GrSearch } from 'react-icons/gr';
 const Header = () => {
   return (
     <div className='siteHeader'>
       {/* ========= 戻るエリア ======== */}
       <div className={'goBackButton'}>TOP画面へ戻る</div>
-      <p>
-        <img src='img/left-icon.png' alt=''></img>
-      </p>
       {/* ======== タイトル画面エリア ======== */}
       <div className={'titleArea'}>商品管理画面</div>
-
       {/* ======== 検索画面エリア ======== */}
-      <div className={'searchArea'}>商品検索画面</div>
-      <input
-        type='search'
-        className='text'
-        placeholder='商品名を入力してください。'
-      />
+      <div className={'searchArea'}>
+        <GrSearch className='GrSearch' />
+        <input
+          type='search'
+          className='text'
+          placeholder='商品名を入力してください。'
+        />
+      </div>
     </div>
   );
 };
