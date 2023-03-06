99eadd16c0ed2b41a6fb9125bbdfd424222049c0
furu0202 <furuka.0202@gmail.com>
2023-03-06 23:44:21 +0900

    TOP画面へ戻るアイコンの実装

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'src/styles/header.css';
+import { HiOutlineHome } from 'react-icons/hi';
 import { GrSearch } from 'react-icons/gr';
 const Header = () => {
   return (
     <div className={'siteHeader'}>
       {/* ========= 戻るエリア ======== */}
       <div className={'goBackButton'}>TOP画面へ戻る</div>
+      <HiOutlineHome className={'HiOutlineHome'} />
       {/* ======== タイトル画面エリア ======== */}
       <div className={'titleArea'}>商品管理画面</div>
       {/* ======== 検索画面エリア ======== */}
