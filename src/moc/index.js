79651be7b7fcd7bd69c6329be0424553e124de43
furu0202 <furuka.0202@gmail.com>
2023-03-12 02:41:13 +0900

    仮データ作成用のmocList作成

---
+++ src/moc/index.js
@@ -0,0 +1,39 @@
+/** リストを表示するための仮データ */
+export const mocList = [
+  {
+    itemCode: 'XXXX-000099',
+    itemName: 'シューズ',
+    category: '備品',
+    stock: 1,
+  },
+  {
+    itemCode: 'XXXX-000111',
+    itemName: 'マイナスドライバー',
+    category: '締めつけ工具',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000111',
+    itemName: 'マイナスドライバー',
+    category: '締めつけ工具',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000111',
+    itemName: 'マイナスドライバー',
+    category: '締めつけ工具',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000111',
+    itemName: 'マイナスドライバー',
+    category: '締めつけ工具',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000111',
+    itemName: 'マイナスドライバー',
+    category: '締めつけ工具',
+    stock: 20,
+  },
+];
