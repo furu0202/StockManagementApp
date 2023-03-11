924421053efd837bc3fdd712adf1afca5d70e6f9
furu0202 <furuka.0202@gmail.com>
2023-03-12 00:40:32 +0900

    テーブル雛形作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
+import 'src/styles/main.css';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <table border='1'>
+        <tr>
+          <th>aaaaa</th>
+          <th>9999</th>
+          <th>0000</th>
+        </tr>
+        <tr>
+          <td>ああああ</td>
+          <td>2222</td>
+          <td>7777</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Main;
