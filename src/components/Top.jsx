fd890f0c9359b68539015cbecf9fbc64240eab94
furu0202 <furuka.0202@gmail.com>
2023-03-16 22:55:37 +0900

    検索フォーム外枠の修正

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -5,8 +5,11 @@ const Top = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <h1>TOP画面</h1>
-      <button onClick={() => navigate('/Home/')}>商品管理画面へ戻る</button>
+      <h1>商品在庫管理画面</h1>
+      <button onClick={() => navigate('/Home/')}>商品管理画面へ</button>
+      <button>商品在庫管理画面へ</button>
+      <button>商品登録画面へ</button>
+      <button>設定画面へ</button>
     </div>
   );
 };
