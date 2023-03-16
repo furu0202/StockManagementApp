8490e4265bc05d461c3e4d98b3e83b528fd43e05
furu0202 <furuka.0202@gmail.com>
2023-03-16 22:43:56 +0900

    useNavigate 削除

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Header from 'src/components/Header';
 import Main from 'src/components/Main';
 
 const Home = () => {
-  const navigate = useNavigate();
   return (
     <div>
       <Header />
