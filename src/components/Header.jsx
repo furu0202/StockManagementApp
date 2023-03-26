e57019bbcbffaaff3a14a0e55d1279f43d36dace
furu0202 <furuka.0202@gmail.com>
2023-03-26 22:34:14 +0900

    TOP画面へ戻るアイコンを削除

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'src/styles/header.css';
-import { HiOutlineHome } from 'react-icons/hi';
 import { GrSearch } from 'react-icons/gr';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,7 +11,6 @@ const Header = () => {
       <div className={'goBackButton'}>
         <button onClick={() => navigate('/')}>TOP画面へ戻る</button>
       </div>
-      <HiOutlineHome className={'HiOutlineHome'} />
       {/* ======== タイトル画面エリア ======== */}
       <div className={'titleArea'}>商品管理画面</div>
       {/* ======== 検索画面エリア ======== */}
