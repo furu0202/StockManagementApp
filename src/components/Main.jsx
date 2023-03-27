89132bdda50c82e6574f3b1de6d34e4dc77c7370
furu0202 <furuka.0202@gmail.com>
2023-03-27 23:55:11 +0900

    <ttble>要素に<tbody>要素を追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,24 +6,26 @@ const Main = () => {
   return (
     <div>
       <table border='1'>
-        <tr>
-          <th>商品コード</th>
-          <th>分類</th>
-          <th>商品名</th>
-          <th>在庫数</th>
-          <th>最終発注日</th>
-        </tr>
-        {mocList.map((listData, key) => {
-          return (
-            <tr key={key} className={key % 2 === 0 ? '' : 'bg-gray'}>
-              <td>{listData.itemCode}</td>
-              <td>{listData.category}</td>
-              <td>{listData.itemName}</td>
-              <td>{listData.stock}</td>
-              <td>{listData.lastOrder}</td>
-            </tr>
-          );
-        })}
+        <tbody>
+          <tr>
+            <th>商品コード</th>
+            <th>分類</th>
+            <th>商品名</th>
+            <th>在庫数</th>
+            <th>最終発注日</th>
+          </tr>
+          {mocList.map((listData, key) => {
+            return (
+              <tr key={key} className={key % 2 === 0 ? '' : 'bg-gray'}>
+                <td>{listData.itemCode}</td>
+                <td>{listData.category}</td>
+                <td>{listData.itemName}</td>
+                <td>{listData.stock}</td>
+                <td>{listData.lastOrder}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
