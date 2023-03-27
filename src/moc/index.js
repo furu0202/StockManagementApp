b3a69d4ef3ba6d290c0ee96fa61630b2e3588a51
furu0202 <furuka.0202@gmail.com>
2023-03-27 23:40:40 +0900

    仮データの表示数を6個から10個へ変更

--- src/moc/index.js
+++ src/moc/index.js
@@ -1,37 +1,61 @@
 /** リストを表示するための仮データ */
 export const mocList = [
   {
-    itemCode: 'XXXX-000099',
+    itemCode: 'XXXX-000000',
     category: '備品',
     itemName: 'シューズ',
     stock: 1,
   },
   {
-    itemCode: 'XXXX-000111',
+    itemCode: 'XXXX-000001',
     category: '締めつけ工具',
     itemName: 'マイナスドライバー',
     stock: 20,
   },
   {
-    itemCode: 'XXXX-000111',
+    itemCode: 'XXXX-000002',
     category: '締めつけ工具',
     itemName: 'マイナスドライバー',
     stock: 20,
   },
   {
-    itemCode: 'XXXX-000111',
+    itemCode: 'XXXX-000003',
     category: '締めつけ工具',
     itemName: 'マイナスドライバー',
     stock: 20,
   },
   {
-    itemCode: 'XXXX-000111',
+    itemCode: 'XXXX-000004',
     category: '締めつけ工具',
     itemName: 'マイナスドライバー',
     stock: 20,
   },
   {
-    itemCode: 'XXXX-000111',
+    itemCode: 'XXXX-000005',
+    category: '締めつけ工具',
+    itemName: 'マイナスドライバー',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000006',
+    category: '締めつけ工具',
+    itemName: 'マイナスドライバー',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000007',
+    category: '締めつけ工具',
+    itemName: 'マイナスドライバー',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000008',
+    category: '締めつけ工具',
+    itemName: 'マイナスドライバー',
+    stock: 20,
+  },
+  {
+    itemCode: 'XXXX-000009',
     category: '締めつけ工具',
     itemName: 'マイナスドライバー',
     stock: 20,
