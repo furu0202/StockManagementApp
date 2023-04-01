c520de532563ad9098784b396682fb6f9788a655
furu0202 <furuka.0202@gmail.com>
2023-04-01 23:18:31 +0900

    TOP画面へ戻るボタンが必要になったため再度追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,9 @@ const Header = () => {
   return (
     <div className={'siteHeader'}>
       {/* ========= TOP画面へ戻るエリア ======== */}
-      <div className={'goBackButton'}>TOP画面へ戻る</div>
+      <div className={'goBackButton'}>
+        <button onClick={() => navigate('/')}>TOP画面へ戻る</button>
+      </div>
       {/* ======== タイトル画面エリア ======== */}
       <div className={'titleArea'}>商品管理画面</div>
       {/* ======== 検索画面エリア ======== */}
